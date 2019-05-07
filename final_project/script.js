9628f86f774ea930ec24db0995fccdd794c6ad78
Maxrua <42980621+Maxrua@users.noreply.github.com>
2019-05-07 09:08:01 +0800

    final_project

---
+++ final_project/script.js
@@ -0,0 +1,9 @@
+function black(obj){
+    obj.style.backgroundColor="black"
+}
+function red(obj){
+    obj.innerhtml=<img src="https://fontmeme.com/permalink/190502/495a9536c359dabc3ce74e0a5fa2f0d9.png" alt="supreme-font" border="0">
+}
+function CT(id){
+    id.innerHTML="ABOUT SUPREME" ;
+}
